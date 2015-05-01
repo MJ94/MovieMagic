9563b89c9973478d7348b8ab65a0a41d83f3ffa6
Michael Jackson <mj94@mj94.net>
2015-05-01 14:35:40 -0500

    Start Main Activity code - errors present

--- app/src/main/java/edu/css/mjackson1/moviemagic/MainActivity.java
+++ app/src/main/java/edu/css/mjackson1/moviemagic/MainActivity.java
@@ -1,39 +1,30 @@
 package edu.css.mjackson1.moviemagic;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    EditText etMovieTitle;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void OnCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etMovieTitle = (EditText) findViewById(R.id.etMovieTitle);
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onClick (View v) {
+        // declare an intent for our new activity
+        Intent i = new Intent(this, ResultsActivity.class);
+        i.putExtra("Title", etMovieTitle.getText().toString());
+        startActivity(i); // display new activity
     }
 }
