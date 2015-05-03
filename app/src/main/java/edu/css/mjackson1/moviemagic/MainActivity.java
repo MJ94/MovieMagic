30f833c3e461fe6adeae7e190f185e3746ea9913
Michael Jackson <mj94@mj94.net>
2015-05-02 19:08:03 -0500

    Start work on second activity and fix errors.

--- app/src/main/java/edu/css/mjackson1/moviemagic/MainActivity.java
+++ app/src/main/java/edu/css/mjackson1/moviemagic/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
 
     EditText etMovieTitle;
 
-    @Override
+
     protected void OnCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
